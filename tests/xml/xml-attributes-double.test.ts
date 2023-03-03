8611abf0813a64c93b54a5fc47d035c6efa04008
Andrew K <andrewjk@gmail.com>
2023-03-03 16:47:02 +1100

    Initial commit

---
+++ tests/xml/xml-attributes-double.test.ts
@@ -0,0 +1,33 @@
+import { test } from "uvu";
+import * as assert from "uvu/assert";
+import grabXml from "../../src/grabXml";
+import { XmlNodeType } from "../../types/XmlNode";
+import sanitizeNode from "../sanitizeNode";
+
+test("XML with double-quoted attributes", () => {
+  const xml = `<xml attribute="value" attribute2="value 2" />`;
+
+  const doc = grabXml(xml);
+  sanitizeNode(doc);
+
+  const expected = {
+    type: XmlNodeType.ELEMENT,
+    tagName: "#root",
+    attributes: {},
+    children: [
+      {
+        type: XmlNodeType.ELEMENT,
+        tagName: "xml",
+        attributes: {
+          attribute: "value",
+          attribute2: "value 2",
+        },
+        children: [],
+      },
+    ],
+  };
+
+  assert.equal(doc, expected);
+});
+
+test.run();
