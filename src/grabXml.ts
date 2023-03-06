a2fa84697a72f570af4acd6ccc962d4a2626d542
Andrew K <andrewjk@gmail.com>
2023-03-06 14:55:51 +1100

    Perf: use charCodeAt, reverse switches and inline parseChar

--- src/grabXml.ts
+++ src/grabXml.ts
@@ -22,11 +22,23 @@ enum ParseLocation {
 
 interface ParseState {
   location: ParseLocation;
-  break: number;
+  start: number;
   node: XmlNode;
   attribute: string;
   quote: string;
 }
+
+const openBracketCode = "<".charCodeAt(0);
+const closeBracketCode = ">".charCodeAt(0);
+const slashCode = "/".charCodeAt(0);
+const equalsCode = "=".charCodeAt(0);
+const singleQuoteCode = "'".charCodeAt(0);
+const doubleQuoteCode = '"'.charCodeAt(0);
+const spaceCode = " ".charCodeAt(0);
+const tabCode = "\t".charCodeAt(0);
+const carriageReturnCode = "\r".charCodeAt(0);
+const newLineCode = "\n".charCodeAt(0);
+const questionCode = "?".charCodeAt(0);
 
 export default function grabXml(content: string) {
   const root: XmlElementNode = {
@@ -39,224 +51,260 @@ export default function grabXml(content: string) {
 
   const state: ParseState = {
     location: ParseLocation.NONE,
-    break: 0,
+    start: 0,
     node: root,
     attribute: "",
     quote: "",
   };
 
   for (let i = 0; i < content.length; i++) {
-    processChar(content, i, state);
+    switch (state.location) {
+      case ParseLocation.NONE: {
+        switch (content.charCodeAt(i)) {
+          case openBracketCode: {
+            updateState(state, ParseLocation.NODE_OPENED, i);
+            break;
+          }
+          case spaceCode:
+          case tabCode:
+          case carriageReturnCode:
+          case newLineCode: {
+            const child: XmlTextNode = {
+              type: XmlNodeType.TEXT,
+              parent: state.node,
+              text: "",
+            };
+            (state.node as XmlElementNode).children.push(child);
+            updateState(state, ParseLocation.INSIDE_TEXT, i, child);
+            break;
+          }
+          default: {
+            const child: XmlTextNode = {
+              type: XmlNodeType.TEXT,
+              parent: state.node,
+              text: "",
+            };
+            (state.node as XmlElementNode).children.push(child);
+            updateState(state, ParseLocation.INSIDE_TEXT, i, child);
+            break;
+          }
+        }
+        break;
+      }
+      case ParseLocation.NODE_OPENED: {
+        switch (content.charCodeAt(i)) {
+          case slashCode: {
+            updateState(state, ParseLocation.NODE_CLOSING, i);
+            break;
+          }
+          case spaceCode:
+          case tabCode:
+          case carriageReturnCode:
+          case newLineCode: {
+            break;
+          }
+          default: {
+            const child: XmlElementNode = {
+              type: XmlNodeType.ELEMENT,
+              parent: state.node,
+              tagName: "",
+              attributes: {},
+              children: [],
+            };
+            (state.node as XmlElementNode).children.push(child);
+            updateState(state, ParseLocation.NODE_OPEN_NAME, i, child);
+            break;
+          }
+        }
+        break;
+      }
+      case ParseLocation.NODE_CLOSING: {
+        updateState(state, ParseLocation.NODE_CLOSE_NAME, i);
+        break;
+      }
+      case ParseLocation.NODE_SELF_CLOSING: {
+        switch (content.charCodeAt(i)) {
+          case closeBracketCode: {
+            updateState(state, ParseLocation.NONE, i, state.node.parent);
+            break;
+          }
+        }
+        break;
+      }
+      case ParseLocation.NODE_OPEN_NAME: {
+        switch (content.charCodeAt(i)) {
+          case closeBracketCode: {
+            (state.node as XmlElementNode).tagName = content.substring(
+              state.start,
+              i
+            );
+            updateState(state, ParseLocation.NONE, i);
+            break;
+          }
+          case spaceCode:
+          case tabCode:
+          case carriageReturnCode:
+          case newLineCode: {
+            (state.node as XmlElementNode).tagName = content.substring(
+              state.start,
+              i
+            );
+            updateState(state, ParseLocation.INSIDE_NODE, i);
+            break;
+          }
+        }
+        break;
+      }
+      case ParseLocation.NODE_CLOSE_NAME: {
+        switch (content.charCodeAt(i)) {
+          case closeBracketCode: {
+            // TODO: Check that the tagname matches
+            updateState(state, ParseLocation.NONE, i, state.node.parent);
+            break;
+          }
+        }
+        break;
+      }
+      case ParseLocation.INSIDE_NODE: {
+        switch (content.charCodeAt(i)) {
+          case slashCode: {
+            updateState(state, ParseLocation.NODE_SELF_CLOSING, i);
+            break;
+          }
+          case closeBracketCode: {
+            // TODO: Handle other types of automatically self-closing nodes, like <link> in HTML
+            if ((state.node as XmlElementNode).tagName.startsWith("?")) {
+              state.node = state.node.parent;
+            }
+            updateState(state, ParseLocation.NONE, i);
+            break;
+          }
+          case questionCode: {
+            // Ignore this -- it's probably at the end of the xml definition node and shouldn't cause a
+            // change in the parse state
+            break;
+          }
+          case spaceCode:
+          case tabCode:
+          case carriageReturnCode:
+          case newLineCode: {
+            break;
+          }
+          default: {
+            updateState(state, ParseLocation.ATTRIBUTE_NAME, i);
+            break;
+          }
+        }
+        break;
+      }
+      case ParseLocation.ATTRIBUTE_NAME: {
+        switch (content.charCodeAt(i)) {
+          case equalsCode: {
+            state.attribute = content.substring(state.start, i);
+            updateState(state, ParseLocation.BEFORE_ATTRIBUTE_VALUE, i);
+            break;
+          }
+          case closeBracketCode: {
+            (state.node as XmlElementNode).attributes[
+              content.substring(state.start, i)
+            ] = "";
+            updateState(state, ParseLocation.NONE, i);
+            break;
+          }
+          case spaceCode:
+          case tabCode:
+          case carriageReturnCode:
+          case newLineCode: {
+            state.attribute = content.substring(state.start, i);
+            updateState(state, ParseLocation.AFTER_ATTRIBUTE_NAME, i);
+            break;
+          }
+        }
+        break;
+      }
+      case ParseLocation.AFTER_ATTRIBUTE_NAME: {
+        switch (content.charCodeAt(i)) {
+          case equalsCode: {
+            updateState(state, ParseLocation.BEFORE_ATTRIBUTE_VALUE, i);
+            break;
+          }
+          case slashCode: {
+            (state.node as XmlElementNode).attributes[state.attribute] = "";
+            updateState(state, ParseLocation.NODE_SELF_CLOSING, i);
+            break;
+          }
+          default: {
+            (state.node as XmlElementNode).attributes[state.attribute] = "";
+            updateState(state, ParseLocation.ATTRIBUTE_NAME, i);
+            break;
+          }
+        }
+        break;
+      }
+      case ParseLocation.BEFORE_ATTRIBUTE_VALUE: {
+        switch (content.charCodeAt(i)) {
+          case singleQuoteCode:
+          case doubleQuoteCode: {
+            state.quote = content[i];
+            updateState(state, ParseLocation.ATTRIBUTE_VALUE, i + 1);
+            break;
+          }
+          case spaceCode:
+          case tabCode:
+          case carriageReturnCode:
+          case newLineCode: {
+            break;
+          }
+          default: {
+            state.quote = "";
+            updateState(state, ParseLocation.ATTRIBUTE_VALUE, i);
+            break;
+          }
+        }
+        break;
+      }
+      case ParseLocation.ATTRIBUTE_VALUE: {
+        switch (content.charCodeAt(i)) {
+          case singleQuoteCode:
+          case doubleQuoteCode: {
+            if (state.quote === content[i]) {
+              (state.node as XmlElementNode).attributes[state.attribute] =
+                content.substring(state.start, i);
+              updateState(state, ParseLocation.INSIDE_NODE, i);
+            }
+            break;
+          }
+          case spaceCode:
+          case tabCode:
+          case carriageReturnCode:
+          case newLineCode: {
+            if (!state.quote) {
+              (state.node as XmlElementNode).attributes[state.attribute] =
+                content.substring(state.start, i);
+              updateState(state, ParseLocation.INSIDE_NODE, i);
+            }
+            break;
+          }
+        }
+        break;
+      }
+      case ParseLocation.INSIDE_TEXT: {
+        switch (content.charCodeAt(i)) {
+          case openBracketCode: {
+            (state.node as XmlTextNode).text = content.substring(
+              state.start,
+              i
+            );
+            updateState(state, ParseLocation.NODE_OPENED, i, state.node.parent);
+            break;
+          }
+        }
+        break;
+      }
+    }
   }
-  processChar(content, content.length + 1, state);
 
   return root;
-}
-
-function processChar(content: string, i: number, state: ParseState) {
-  switch (content[i]) {
-    case "<": {
-      switch (state.location) {
-        case ParseLocation.NONE: {
-          updateState(state, ParseLocation.NODE_OPENED, i);
-          break;
-        }
-        case ParseLocation.INSIDE_TEXT: {
-          (state.node as XmlTextNode).text = content.substring(state.break, i);
-          updateState(state, ParseLocation.NODE_OPENED, i, state.node.parent);
-          break;
-        }
-      }
-      break;
-    }
-    case "/": {
-      switch (state.location) {
-        case ParseLocation.NODE_OPENED: {
-          updateState(state, ParseLocation.NODE_CLOSING, i);
-          break;
-        }
-        case ParseLocation.INSIDE_NODE: {
-          updateState(state, ParseLocation.NODE_SELF_CLOSING, i);
-          break;
-        }
-        case ParseLocation.AFTER_ATTRIBUTE_NAME: {
-          (state.node as XmlElementNode).attributes[state.attribute] = "";
-          updateState(state, ParseLocation.NODE_SELF_CLOSING, i);
-          break;
-        }
-      }
-      break;
-    }
-    case ">": {
-      switch (state.location) {
-        case ParseLocation.NODE_SELF_CLOSING: {
-          updateState(state, ParseLocation.NONE, i, state.node.parent);
-          break;
-        }
-        case ParseLocation.NODE_OPEN_NAME: {
-          (state.node as XmlElementNode).tagName = content.substring(
-            state.break,
-            i
-          );
-          updateState(state, ParseLocation.NONE, i);
-          break;
-        }
-        case ParseLocation.NODE_CLOSE_NAME: {
-          // TODO: Check that the tagname matches
-          updateState(state, ParseLocation.NONE, i, state.node.parent);
-          break;
-        }
-        case ParseLocation.INSIDE_NODE: {
-          // TODO: Handle other types of automatically self-closing nodes, like <link> in HTML
-          if ((state.node as XmlElementNode).tagName.startsWith("?")) {
-            state.node = state.node.parent;
-          }
-          updateState(state, ParseLocation.NONE, i);
-          break;
-        }
-        case ParseLocation.ATTRIBUTE_NAME: {
-          (state.node as XmlElementNode).attributes[
-            content.substring(state.break, i)
-          ] = "";
-          updateState(state, ParseLocation.NONE, i);
-          break;
-        }
-      }
-      break;
-    }
-    case "=": {
-      switch (state.location) {
-        case ParseLocation.ATTRIBUTE_NAME: {
-          state.attribute = content.substring(state.break, i);
-          updateState(state, ParseLocation.BEFORE_ATTRIBUTE_VALUE, i);
-          break;
-        }
-        case ParseLocation.AFTER_ATTRIBUTE_NAME: {
-          updateState(state, ParseLocation.BEFORE_ATTRIBUTE_VALUE, i);
-          break;
-        }
-      }
-      break;
-    }
-    case '"':
-    case "'": {
-      // TODO: Support unquoted attribute values too
-      switch (state.location) {
-        case ParseLocation.BEFORE_ATTRIBUTE_VALUE: {
-          state.quote = content[i];
-          updateState(state, ParseLocation.ATTRIBUTE_VALUE, i + 1);
-          break;
-        }
-        case ParseLocation.ATTRIBUTE_VALUE: {
-          if (state.quote === content[i]) {
-            (state.node as XmlElementNode).attributes[state.attribute] =
-              content.substring(state.break, i);
-            updateState(state, ParseLocation.INSIDE_NODE, i);
-          }
-          break;
-        }
-      }
-      break;
-    }
-    case " ":
-    case "\t":
-    case "\r":
-    case "\n": {
-      switch (state.location) {
-        case ParseLocation.NONE: {
-          const child: XmlTextNode = {
-            type: XmlNodeType.TEXT,
-            parent: state.node,
-            text: "",
-          };
-          (state.node as XmlElementNode).children.push(child);
-          updateState(state, ParseLocation.INSIDE_TEXT, i, child);
-          break;
-        }
-        case ParseLocation.NODE_OPEN_NAME: {
-          (state.node as XmlElementNode).tagName = content.substring(
-            state.break,
-            i
-          );
-          updateState(state, ParseLocation.INSIDE_NODE, i);
-          break;
-        }
-        case ParseLocation.ATTRIBUTE_NAME: {
-          state.attribute = content.substring(state.break, i);
-          updateState(state, ParseLocation.AFTER_ATTRIBUTE_NAME, i);
-          break;
-        }
-        case ParseLocation.ATTRIBUTE_VALUE: {
-          if (!state.quote) {
-            (state.node as XmlElementNode).attributes[state.attribute] =
-              content.substring(state.break, i);
-            updateState(state, ParseLocation.INSIDE_NODE, i);
-          }
-          break;
-        }
-      }
-      break;
-    }
-    case undefined: {
-      switch (state.location) {
-        case ParseLocation.INSIDE_TEXT: {
-          (state.node as XmlTextNode).text = content.substring(state.break, i);
-          updateState(state, ParseLocation.NODE_OPENED, i, state.node.parent);
-          break;
-        }
-      }
-      break;
-    }
-    case "?": {
-      // Ignore this -- it's probably at the end of the xml definition node and shouldn't cause a
-      // change in the parse state
-      break;
-    }
-    default: {
-      switch (state.location) {
-        case ParseLocation.NONE: {
-          const child: XmlTextNode = {
-            type: XmlNodeType.TEXT,
-            parent: state.node,
-            text: "",
-          };
-          (state.node as XmlElementNode).children.push(child);
-          updateState(state, ParseLocation.INSIDE_TEXT, i, child);
-          break;
-        }
-        case ParseLocation.NODE_OPENED: {
-          const child: XmlElementNode = {
-            type: XmlNodeType.ELEMENT,
-            parent: state.node,
-            tagName: "",
-            attributes: {},
-            children: [],
-          };
-          (state.node as XmlElementNode).children.push(child);
-          updateState(state, ParseLocation.NODE_OPEN_NAME, i, child);
-          break;
-        }
-        case ParseLocation.NODE_CLOSING: {
-          updateState(state, ParseLocation.NODE_CLOSE_NAME, i);
-          break;
-        }
-        case ParseLocation.INSIDE_NODE: {
-          updateState(state, ParseLocation.ATTRIBUTE_NAME, i);
-          break;
-        }
-        case ParseLocation.AFTER_ATTRIBUTE_NAME: {
-          (state.node as XmlElementNode).attributes[state.attribute] = "";
-          updateState(state, ParseLocation.ATTRIBUTE_NAME, i);
-          break;
-        }
-        case ParseLocation.BEFORE_ATTRIBUTE_VALUE: {
-          state.quote = "";
-          updateState(state, ParseLocation.ATTRIBUTE_VALUE, i);
-          break;
-        }
-      }
-    }
-  }
 }
 
 function updateState(
@@ -266,7 +314,7 @@ function updateState(
   node?: XmlNode
 ) {
   state.location = location;
-  state.break = breakPos;
+  state.start = breakPos;
   if (node) {
     state.node = node;
   }
